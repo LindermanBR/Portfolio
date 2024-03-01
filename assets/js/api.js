866983c4d03dccefcb853fb0f8dea4e0a70b4c28
Linderman Moura <linderman.moura@outlook.com>
2024-03-01 02:18:24 -0300

    Image Changes

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 async function fetchProfileData() {
   const url =
-    "https://raw.githubusercontent.com/LindermanBR/Portfolio/main/data/profile.json?token=GHSAT0AAAAAACH3ZOXG5SP4L7NIWECPGVXEZPBMCDA";
+    "https://raw.githubusercontent.com/LindermanBR/Portfolio/main/data/profile.json?token=GHSAT0AAAAAACH3ZOXGQVTVASX2MWVWAJKQZPBN7HQ";
   const fetching = await fetch(url);
 
   return await fetching.json();
