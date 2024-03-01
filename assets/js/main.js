261f77adfe278c97c6164f8bf510cef3a4a973ef
Linderman Moura <linderman.moura@outlook.com>
2024-03-01 03:04:09 -0300

    Final Touch

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,10 +35,62 @@ function updateHardSkills(profileData) {
   const hardSkills = document.getElementById("profile-hard-skills");
 
   profileData.skills.hardSkills.map((skill) => {
-    console.log(skill.logo);
     const skillElement = document.createElement("li");
     skillElement.innerHTML = `<img src="${skill.logo}" alt="${skill.alt}" title="${skill.alt}">`;
     hardSkills.appendChild(skillElement);
+  });
+}
+
+function updateLanguages(profileData) {
+  const languages = document.getElementById("profile-languages");
+
+  profileData.languages.map((language) => {
+    const languageElement = document.createElement("li");
+    languageElement.innerText = language;
+    languages.appendChild(languageElement);
+  });
+}
+
+function updatePortfolio(profileData) {
+  const portfolio = document.getElementById("profile-portfolio");
+
+  profileData.portfolio.map((project) => {
+    const liElement = document.createElement("li");
+    const titleElement = document.createElement("h3");
+    const linkElement = document.createElement("a");
+
+    titleElement.innerText = project.name;
+    titleElement.classList.add("title");
+    project.github ? titleElement.classList.add("github") : null;
+
+    linkElement.innerText = "Link Repositório";
+    linkElement.href = project.url;
+    linkElement.target = "_blank";
+    liElement.appendChild(titleElement);
+    liElement.appendChild(linkElement);
+    portfolio.appendChild(liElement);
+    console.log(project);
+  });
+}
+
+function updateExperience(profileData) {
+  const experience = document.getElementById("profile-experience");
+
+  profileData.professionalExperience.map((job) => {
+    const liElement = document.createElement("li");
+    const titleElement = document.createElement("h3");
+    const periodElement = document.createElement("p");
+    const descriptionElement = document.createElement("p");
+
+    titleElement.innerText = job.name;
+    periodElement.innerText = job.period;
+    periodElement.classList.add("period");
+    descriptionElement.innerText = job.description;
+    liElement.appendChild(titleElement);
+    liElement.appendChild(periodElement);
+    liElement.appendChild(descriptionElement);
+    experience.appendChild(liElement);
+    console.log(job);
   });
 }
 
@@ -47,5 +99,7 @@ function updateHardSkills(profileData) {
   updateProfileInfo(profileData);
   updateSoftSkills(profileData);
   updateHardSkills(profileData);
-  console.log(profileData);
+  updateLanguages(profileData);
+  updatePortfolio(profileData);
+  updateExperience(profileData);
 })();
